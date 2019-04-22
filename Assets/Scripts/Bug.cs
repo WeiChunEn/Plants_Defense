442c23175ba7bb44081ffa94ada6297a208bdbdd
WeiChunEn <sa510241422as@gmail.com>
2019-04-22 22:42:50 +0800

    Fourth Commit

---
+++ Assets/Scripts/Bug.cs
@@ -0,0 +1,148 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.AI;
+
+[RequireComponent(typeof(NavMeshAgent))]
+
+public class Bug : MonoBehaviour
+{
+
+    [Header("方便觀看")]
+    public string _sBug_Name;
+    public float _fBug_Speed;
+    public int _iBug_HP;
+    public int _iBug_Money;
+    public Slider _sBug_HP;
+    public GameObject _gTarget;
+
+    public class Bug_Data
+    {
+        protected string _sBug_Name;
+
+        protected float _fBug_Speed;
+        protected int _iBug_HP;
+        protected int _iBug_Money;
+    }
+
+    public class Set_Bug : Bug_Data
+    {
+        public Set_Bug(string Bug_Name, int Bug_HP, float Bug_Speed, int Bug_Money)
+        {
+            this._sBug_Name = Bug_Name;
+            this._iBug_HP = Bug_HP;
+            this._fBug_Speed = Bug_Speed;
+            this._iBug_Money = Bug_Money;
+        }
+
+        public string Bug_Name
+        {
+            get
+            {
+                return this._sBug_Name;
+            }
+        }
+
+        public int Bug_HP
+        {
+            get
+            {
+                return this._iBug_HP;
+            }
+
+            set
+            {
+                _iBug_HP = value;
+                if (_iBug_HP < 0)
+                {
+                    _iBug_HP = 0;
+                }
+            }
+        }
+
+        public float Bug_Speed
+        {
+            get
+            {
+                return this._fBug_Speed;
+            }
+
+            set
+            {
+                _fBug_Speed = value;
+            }
+        }
+
+        public int Bug_Money
+        {
+            get
+            {
+                return this._iBug_Money;
+            }
+            set
+            {
+                _iBug_Money = value;
+            }
+        }
+    }
+
+    Set_Bug Insect;
+    public NavMeshAgent navMeshAgent;
+    // Start is called before the first frame update
+    private void Awake()
+    {
+        
+        navMeshAgent = GetComponent<NavMeshAgent>();
+        _gTarget = GameObject.Find("Player");
+       
+    }
+
+    void Start()
+    {
+        Set_Insect();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug();
+        
+        navMeshAgent.SetDestination(_gTarget.transform.position);
+    }
+
+    public void Debug()
+    {
+        _iBug_HP = Insect.Bug_HP;
+        _fBug_Speed = Insect.Bug_Speed;
+
+    }
+
+    public void Set_Insect()
+    {
+        switch (gameObject.name)
+        {
+           
+            case "Bee(Clone)":
+                print(123);
+                _sBug_Name = "Bee";
+                _iBug_HP = 5;
+                _fBug_Speed = 5.0f;
+                navMeshAgent.speed = _fBug_Speed;
+                _iBug_Money = 5;
+                Insect = new Set_Bug(_sBug_Name, _iBug_HP, _fBug_Speed, _iBug_Money);
+                break;
+            case "Ant(Clone)":
+                _sBug_Name = "Ant";
+                _iBug_HP = 3;
+                _fBug_Speed = 3.0f;
+                navMeshAgent.speed = _fBug_Speed;
+                _iBug_Money = 3;
+                Insect = new Set_Bug(_sBug_Name, _iBug_HP, _fBug_Speed, _iBug_Money);
+                break;
+
+
+
+        }
+    }
+}
---
+++ Assets/Scripts/Randon_Bug.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Randon_Bug : MonoBehaviour
+{
+    public GameObject[] _gBug;
+    
+    public GameObject  _gBug_Point;
+   
+    public GameObject _gBug_Area;
+
+    
+   
+    public int _iRandon_Bug;
+    
+    public float _fStart_Time;
+    public float _fRandom_Time;
+
+    private void Awake()
+    {
+        _fStart_Time = 0.0f;
+        _fRandom_Time = 3.0f;
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        _fStart_Time += Time.deltaTime;
+        if (_fStart_Time >= _fRandom_Time)
+        {
+            _fStart_Time = 0.0f;
+            Ins_Bug();
+            
+        }
+    }
+
+    public void Ins_Bug()
+    {
+        
+        _iRandon_Bug = Random.Range(0, 2);
+        Instantiate(_gBug[_iRandon_Bug], _gBug_Point.transform.position, _gBug[_iRandon_Bug].transform.rotation,_gBug_Area.transform);
+    }
+
+   
+}
