7ab5a7361a866943d089c15e2065ca9b77730cbe
WeiChunEn <sa510241422as@gmail.com>
2019-04-24 15:34:29 +0800

    Logo Scene

---
+++ Assets/Plant_Defense/Resources/Logo/Script/HexSphere.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class HexSphere : MonoBehaviour
+{
+    public Renderer m_Renderer;
+    protected float m_OutlineIntensity = 2.0f;
+
+	// Use this for initialization
+	void Start ()
+    {
+        DOTween.To(() => m_OutlineIntensity, x => m_OutlineIntensity = x, 5.0f, 1.0f).SetEase(Ease.OutSine).SetLoops(-1, LoopType.Yoyo);
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		if (m_Renderer != null)
+        {
+            m_Renderer.material.SetFloat("_Outline_Opacity", m_OutlineIntensity);
+        }
+	}
+}
---
+++ Assets/Plant_Defense/Resources/Logo/Script/VRClass/LogoTest.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LogoTest : MonoBehaviour
+{
+    private void Start()
+    {
+        transform.position = Camera.main.transform.position;
+    }
+    // Use this for initialization
+    void Update ()
+    {
+        transform.position = Vector3.Lerp(transform.position, Camera.main.transform.position, Time.deltaTime * 0.5f);
+	}
+}
